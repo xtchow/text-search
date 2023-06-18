4458fe1459ee72a3fae049c2e1057e5f212eef1d
XTCHOW <xtchow2020@gmail.com>
2023-06-18 09:56:51 -0500

    setup test suite, add database

---
+++ src/models/redis.js
@@ -0,0 +1,60 @@
+import { Client, Entity, Schema, Repository } from 'redis-om';
+
+const client = new Client();
+const connect = async function() {
+  if (!client.isOpen()) {
+    await client.open(process.env.REDIS_URL);
+  }
+};
+
+class File extends Entity {}
+let fileSchema = new Schema(File, {
+  modified: { type: 'date' },
+  name: { type: 'string', textSearch: true },
+  mime: { type: 'string' },
+  text: { type: 'text', textSearch: true }
+}, { dataStructure: "JSON" });
+
+// class Redis {
+//   constructor(el) {}
+
+//   async createFile(data) {}
+// }
+
+const redis = {
+  createFile: async function(data) {
+    await connect();
+
+    // https://stackoverflow.com/questions/71926028/redis-om-fastify-this-writeentity-is-not-a-function/71926517#71926517
+    // const repo = new Repository(fileSchema, client);
+    const repo = client.fetchRepository(fileSchema);
+    const file = repo.createEntity(data);
+    const id = await repo.save(file);
+    return id;
+  },
+  createIndex: async function() {
+    await connect();
+    const repo = client.fetchRepository(fileSchema);
+    await repo.createIndex();
+  },
+  searchFiles: async function(query) {
+    await connect();
+
+    const repo = client.fetchRepository(fileSchema);
+    console.log('repo', repo);
+    const files = await repo.search()
+      .where('name').eq(query)
+      .or('text').matches(query)
+      .return.all();
+    return files;
+  },
+  everything: async function() {
+    await connect();
+
+    const repo = client.fetchRepository(fileSchema);
+    const files = await repo.search().return.all();
+    return files;
+  }
+};
+export default redis;
+
